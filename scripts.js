59d393fc0b323b30faaf3fb82214bb824ca712b3
chphuttipan <phuttipan.ch@gmail.com>
2025-07-08 22:22:20 +0700

    16x16 square grids

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
-// 1. Create a 16x16 grid of square divs
+// 1. Create a container DOM and style it
 const container = document.getElementById('container');
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap';
+container.style.width = '512px';
+container.style.height = '512px';
 
-function  createSquareGrid(box, gridSize) {
-
-    for (let i = 0; i < (gridSize * gridSize); i++) {
-        const squareGrid = document.createElement('div');
-        squareGrid.className = 'grid-square';
-        box.appendChild(squareGrid);
-    }
+// 2. Create a 16x16 grid of square divs
+const gridSize = 16
+const totalSquare = gridSize * gridSize;
+const squareWidth = `calc((100% - 1px * (${gridSize} - 1)) / (${gridSize} + 1))`;
+for (let i = 0; i < totalSquare; i++) {
+    const square = document.createElement('div');
+    square.style.width = squareWidth;
+    square.style.height = squareWidth;
+    square.style.border = '1px solid black';
+    square.style.hover = ''
+    container.appendChild(square);
 }
 
-createSquareGrid(container, 16);
+// 3. Change the background color during hover with javascript
